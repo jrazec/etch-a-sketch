bd52570594ddd33bd7449eb8f4c7632d528995bc
jrazec <johnrazecschool@gmail.com>
2023-08-08 09:59:49 +0800

    Create logic to delete the recent cells, then replace with new set(Has Bug)

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-
-
+//to acces anytime anywhere!
+let size;
+const sketchArea = document.querySelector('.sketch-area');
+let base;
+let width;
+//Creation of divs logic AS DEFAULT OF 16
 function changeSize() {
+    size = parseInt(rangeSlider.value);//since it returns string and once u multiply int with string, its not the SAME value!! {try to remove parseInt to see :>}
+    base = 600/size;//formula to get a equal sizes for each cells
+    width = 600+size;//formula to allocate the gaps
+    sketchArea.setAttribute('style', `width:${width}px`);
 
+    for(let i = 0; i < size; i++){
+        for(let j = 0; j < size; j++){
+            const cells = document.createElement('div');
+            cells.classList.add('cell');
+            cells.setAttribute('style', `flex: 1 0 ${base}px`);
+            sketchArea.appendChild(cells);
+        }
+    }
 }
 
 //Printing of range number
@@ -13,20 +29,27 @@ rangeSlider.addEventListener('input', () => {
 });
 
 
-
-//Creation of divs logic
-const sketchArea = document.querySelector('.sketch-area');
-let size = 10;
-const base = 600/size;//formula to get a equal sizes for each cells
-const width = 600+size;//formula to allocate the gaps
-sketchArea.setAttribute('style', `width:${width}px`);
-
-for(let i = 0; i < size; i++){
-    for(let j = 0; j < size; j++){
-        const cells = document.createElement('div');
-        cells.classList.add('cell')
-        cells.setAttribute('style', `flex: 1 0 ${base}px`);
-        sketchArea.appendChild(cells)
+changeSize();
+const applyBtn = document.querySelector('#apply-btn');
+applyBtn.addEventListener('click', () => {
+    for(let i = 0; i < (size*size);i++){
+        sketchArea.removeChild(sketchArea.lastElementChild);//to access the nodelist, childnodes is needed to be used
     }
-}
+    changeSize();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
